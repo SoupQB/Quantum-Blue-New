b7bb454a40919bfa3ced96111367d71e78d9bd2f
RedBastion <77165019+Sage-Jones@users.noreply.github.com>
2026-03-20 23:49:22 -0500

    Killing matt's bat files (#34)
    This reverts commit ae82083ec5e5db78a941c69f94113dfee0eb5810.
    Partially, we keepin the wall fixes I'm just unfucking matt's bat files
    (killing them)

--- runclient-Release.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-dotnet run --project Content.Client --configuration Release
-pause
--- runserver-Release.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-dotnet run --project Content.Server --configuration Release
-read -p "Press enter to continue"
